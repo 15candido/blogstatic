79a26828c1b98e273575037d5a984ba982373e95
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-09-17 21:48:27 +0100

    My first commit.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Project</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <!-- Links  css and js files -->
+    @vite('resources/css/app.css')
+</head>
+
+<body class="w-full min-h-screen bg-white/95 m-auto">
+    <div class="max-w-6xl bg-green-400">
+        <div class="flex flex-col">
+            <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+                deserunt saepe, quasi tenetur quod quas itaque aliquid, optio veniam excepturi consequatur nobis,
+                alias impedit? Veniam aliquam vitae adipisci nesciunt vero?</div>
+            <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+                deserunt saepe, quasi tenetur quod quas itaque aliquid, optio veniam excepturi consequatur nobis,
+                alias impedit? Veniam aliquam vitae adipisci nesciunt vero?</div>
+            <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+                deserunt saepe, quasi tenetur quod quas itaque aliquid, optio veniam excepturi consequatur nobis,
+                alias impedit? Veniam aliquam vitae adipisci nesciunt vero?</div>
+        </div>
+
+        <div class=""></div>
+    </div>
+</body>
+
+</html>
