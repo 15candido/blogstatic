192cf21a75567c4fa965250a41a2655e0e5a757a
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-09-18 22:59:51 +0100

    Guest and App views was created.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+{{$slot}}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+Semi+Condensed&display=swap" rel="stylesheet">
+    <!-- Links  css and js files -->
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    {{-- Menu navigation section --}}
+
+    <head></head>
+
+    {{-- Body section --}}
+    <main class="antialiased w-full min-h-screen bg-white/80">
+        {{$slot}}
+    </main>
+
+    {{-- Footer section --}}
+    <footer></footer>
+
+</body>
+
+</html>
