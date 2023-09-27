372435fc418312181ca78f8d05f9010b552f2cac
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-09-27 22:23:35 +0100

    V27_09_23 Route's and Links has been created for dynamic blog post.

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ Route::get('/', function () {
     return view('posts');
 });
 Route::get('post', function () {
-    return view('post');
+    return view('post', [
+        'post' => file_get_contents(__DIR__ . '/../resources/posts/my-first-post.html') //$post
+    ]);
 });
