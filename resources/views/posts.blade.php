63b42081977ce1a209b641dc74ae8391d1d6d320
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-09-27 22:25:53 +0100

    V27_09_23 Blog's posts list has been created.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 <x-guestLayout>
 
 
-    <div class="max-w-7xl flex mx-auto my-6 gap-6 p-2 ">
+    <div class="max-w-7xl md:flex mx-auto my-6 gap-6 p-2 ">
         <article class="flex flex-col space-y-2 p-3 bg-white shadow-md shadow-gray-300
          rounded-md hover:shadow-lg hover:shadow-gray-400">
             <a href="/post">
