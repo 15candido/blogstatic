d42b2508b6f63950cd88694fda3be51a6bde7e7f
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-10-05 19:31:29 +0100

    V05_10_23_19h30. Dynamic post. Passing a variable stead of static content .

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,14 @@
 <x-guestLayout>
     <div class="max-w-7xl flex mx-auto my-6 gap-6 p-2">
         <!-- <article class="flex flex-col space-y-2 p-3 bg-white shadow-md shadow-gray-300
-         rounded-md hover:shadow-lg hover:shadow-gray-400"> -->
-            <!-- <a href="/post">
+         rounded-md hover:shadow-lg hover:shadow-gray-400">
+            <a href="/post">
                 <h1 class="font-bold text-2xl ">My First Post</h1>
             </a> -->
-            <!-- <p class="leading-6 "> -->
-                <?= $post; ?>
-            <!-- </p> -->
-            <!-- <a href="/" class="font-extrabold text-base uppercase text-red-400">Go Back</a> -->
-        <!-- </article>
-    </div> -->
+        <p class="leading-6 ">
+            <?= $post; ?>
+        </p>
+        <!-- <a href="/" class="font-extrabold text-base uppercase text-red-400">Go Back</a>
+        </article> -->
+    </div>
 </x-guestLayout>
