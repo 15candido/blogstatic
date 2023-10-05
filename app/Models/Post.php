6527bc5dec48ea0c8779dfba5a8a738f10b68378
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-10-05 19:35:11 +0100

    V05_10_23_19h34. Post Models

---
+++ app/Models/Post.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post //extends Model
+{
+    use HasFactory;
+
+
+    public static function all()
+    {
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(function ($file) {
+
+            return $file->getContents();
+        }, $files);
+    }
+
+    public static function find($slug)
+    {
+        $path = resource_path("posts/{$slug}.html"); //$post
+
+        if (!file_exists($path)) {
+            throw new ModelNotFoundException(); // If not found throw exception messager to user 
+        };
+        return cache()->remember("posts.{$slug}", 5, function () use ($path) {
+            return file_get_contents($path);   //var_dump('file_get_contents');
+        });
+    }
+}
