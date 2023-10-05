8448f7aae561919e3ba04bc06deb05a4399c858e
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-10-05 23:02:13 +0100

    V05_10_23_23h02. bg has been removed from h1 and p tag.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,9 @@
             <article class="w-full flex flex-col my-6 space-y-2 p-3 bg-white shadow-md shadow-gray-300 rounded-md hover:shadow-lg
             hover:shadow-gray-400">
                 <a href="/posts/{{$post->slug}}">
-                    <h1 class="font-bold text-2xl bg-red-400">{{$post->title}}</h1>
+                    <h1 class="font-bold text-2xl">{{$post->title}}</h1>
                 </a>
-                <p class="leading-6 bg-red-400">{!! $post->excerpt !!}</p>
+                <p class="leading-6">{!! $post->excerpt !!}</p>
             </article>
         <?php endforeach; ?>
     </div>
