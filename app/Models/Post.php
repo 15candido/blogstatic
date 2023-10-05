4ae75262548cc9e7b2e33ae7e52839abb73a1df7
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-10-05 22:58:12 +0100

    V05_10_23_22h59. Use of collection stead of map or map array.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Symfony\Component\Translation\Dumper\YamlFileDumper;
 
 class Post //extends Model
 {
@@ -25,23 +27,67 @@ class Post //extends Model
 
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
+        // Find all files on Posts directory
+        $files = File::files(resource_path("posts"));
+        //  And collect them in to collection 
+        return collect($files)
+            // Map over on each item and reach one, parse that file into document
+            ->map(function ($file) {
+                return YamlFrontMatter::parseFile($file);
+            })
+            // Once have a collection of documents, map over into second time and build the post
+            ->map(function ($document) {
+                return new Post(
+                    $document->title,
+                    $document->slug,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body()
+                );
+            });
 
-        return array_map(function ($file) {
+        // Use of map arry of objects
+        // $posts = array_map(function ($file) {
 
-            return $file->getContents();
-        }, $files);
+        //     $document = YamlFrontMatter::parseFile($file);
+        //     return new Post(
+        //         $document->title,
+        //         $document->slug,
+        //         $document->excerpt,
+        //         $document->date,
+        //         $document->body()
+        //     );
+        // }, $files);
+
+        // New array 
+        // $posts = [];
+        // foreach ($files as $file) {
+        //     $document =  YamlFrontMatter::parseFile($file);
+
+        //     $posts[] = new Post(
+        //         $document->title,
+        //         $document->slug,
+        //         $document->excerpt,
+        //         $document->date,
+        //         $document->body()
+        //     );
+        // }
     }
 
     public static function find($slug)
     {
-        $path = resource_path("posts/{$slug}.html"); //$post
 
-        if (!file_exists($path)) {
-            throw new ModelNotFoundException(); // If not found throw exception messager to user 
-        };
-        return cache()->remember("posts.{$slug}", 5, function () use ($path) {
-            return file_get_contents($path);   //var_dump('file_get_contents');
-        });
+        // Of all the blog post, find the one with a slug match the one that was requested
+
+        $posts = static::all();
+        return $posts->firstWhere('slug', $slug);
+
+        // $path = resource_path("posts/{$slug}.html"); //$post
+        // if (!file_exists($path)) {
+        //     throw new ModelNotFoundException(); // If not found throw exception messager to user 
+        // };
+        // return cache()->remember("posts.{$slug}", 5, function () use ($path) {
+        //     return file_get_contents($path);   //var_dump('file_get_contents');
+        // });
     }
 }
