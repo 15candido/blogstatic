7eb31ded86f3d2b86ed1c847903e7b770a265986
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-10-05 21:28:36 +0100

    V05_10_23_21h28. Arranging posts's view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,63 +1,13 @@
 <x-guestLayout>
-
-    <?php foreach ($posts as $post) : ?>
-
-        <div class="max-w-7xl flex-row mx-auto my-6 gap-6 p-2">
-
-            <?= $post; ?>
-
+    <div class="max-w-7xl flex mx-auto my-6 gap-6 p-2">
+        <?php foreach ($posts as $post) : ?>
+            <article class="w-full flex flex-col my-6 space-y-2 p-3 bg-white shadow-md shadow-gray-300 rounded-md hover:shadow-lg
+            hover:shadow-gray-400">
+                <a href="/posts/{{$post->slug}}">
+                    <h1 class="font-bold text-2xl bg-red-400">{{$post->title}}</h1>
+                </a>
+                <p class="leading-6 bg-red-400">{!! $post->excerpt !!}</p>
+            </article>
         <?php endforeach; ?>
-
-        <!-- <article class="flex flex-col space-y-2 p-3 bg-white shadow-md shadow-gray-300
-         rounded-md hover:shadow-lg hover:shadow-gray-400">
-            <a href="/posts/my-first-post">
-                <h1 class="font-bold text-2xl ">My First Post</h1>
-            </a>
-            <p class="leading-6 ">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, tempora aliquam.
-                Exercitationem quos, facere, vitae quisquam soluta quas, fuga praesentium mollitia laudantium
-                amet est? Quidem maiores quo deserunt rem quas.
-                Quasi a enim nesciunt fugit tenetur error dolore cumque ex itaque cum quae modi delectus,
-                quidem possimus aspernatur vitae quam quaerat autem incidunt mollitia? Dicta sunt pariatur
-                officia odit consequatur.
-                Illo nisi at harum neque quasi voluptatibus cupiditate. Ea distinctio veritatis atque? Soluta,
-                quos doloremque nemo, vel voluptate laudantium voluptas placeat perferendis saepe maiores hic
-                enim incidunt rem explicabo modi.
-            </p>
-        </article>
-        <article class="flex flex-col space-y-2 p-3 bg-white shadow-md shadow-gray-300
-         rounded-md hover:shadow-lg hover:shadow-gray-400">
-            <a href="posts/my-second-post">
-                <h1 class="font-bold text-2xl ">My Second Post</h1>
-            </a>
-            <p class="leading-6 ">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, tempora aliquam.
-                Exercitationem quos, facere, vitae quisquam soluta quas, fuga praesentium mollitia laudantium
-                amet est? Quidem maiores quo deserunt rem quas.
-                Quasi a enim nesciunt fugit tenetur error dolore cumque ex itaque cum quae modi delectus,
-                quidem possimus aspernatur vitae quam quaerat autem incidunt mollitia? Dicta sunt pariatur
-                officia odit consequatur.
-                Illo nisi at harum neque quasi voluptatibus cupiditate. Ea distinctio veritatis atque? Soluta,
-                quos doloremque nemo, vel voluptate laudantium voluptas placeat perferendis saepe maiores hic
-                enim incidunt rem explicabo modi.
-            </p>
-        </article>
-        <article class="flex flex-col space-y-2 p-3 bg-white shadow-md shadow-gray-300
-         rounded-md hover:shadow-lg hover:shadow-gray-400">
-            <a href="posts/my-third-post">
-                <h1 class="font-bold text-2xl ">My Third Post</h1>
-            </a>
-            <p class="leading-6 ">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, tempora aliquam.
-                Exercitationem quos, facere, vitae quisquam soluta quas, fuga praesentium mollitia laudantium
-                amet est? Quidem maiores quo deserunt rem quas.
-                Quasi a enim nesciunt fugit tenetur error dolore cumque ex itaque cum quae modi delectus,
-                quidem possimus aspernatur vitae quam quaerat autem incidunt mollitia? Dicta sunt pariatur
-                officia odit consequatur.
-                Illo nisi at harum neque quasi voluptatibus cupiditate. Ea distinctio veritatis atque? Soluta,
-                quos doloremque nemo, vel voluptate laudantium voluptas placeat perferendis saepe maiores hic
-                enim incidunt rem explicabo modi.
-            </p>
-        </article> -->
-        </div>
+    </div>
 </x-guestLayout>
