d7064f823f820f0d198050c5b4c8d99d4ba94e50
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-10-07 11:46:32 +0100

    V07_10_23_11h42. Change the posts view to use the syntax structure for vannila PHP.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,13 @@
 <x-guestLayout>
     <div class="max-w-7xl flex mx-auto my-6 gap-6 p-2">
-        <?php foreach ($posts as $post) : ?>
-            <article class="w-full flex flex-col my-6 space-y-2 p-3 bg-white shadow-md shadow-gray-300 rounded-md hover:shadow-lg
+        @foreach ($posts as $post)
+        <article class="w-full flex flex-col my-6 space-y-2 p-3 bg-white shadow-md shadow-gray-300 rounded-md hover:shadow-lg
             hover:shadow-gray-400">
-                <a href="/posts/{{$post->slug}}">
-                    <h1 class="font-bold text-2xl">{{$post->title}}</h1>
-                </a>
-                <p class="leading-6">{!! $post->excerpt !!}</p>
-            </article>
-        <?php endforeach; ?>
+            <a href="/posts/{{$post->slug}}">
+                <h1 class="font-bold text-2xl">{{$post->title}}</h1>
+            </a>
+            <p class="leading-6">{!! $post->excerpt !!}</p>
+        </article>
+        @endforeach
     </div>
 </x-guestLayout>
